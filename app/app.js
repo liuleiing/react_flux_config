2b252fc3b8977c799f447d1a53bca1e24eb1a6dd
liuleiing <1102820597@qq.com>
2017-03-07 10:55:20 +0800

    react_flux开发环境配置

---
+++ app/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Normalize.css(重置样式) amazeui自带
+import 'amazeui-touch/dist/amazeui.touch.min.css';
+ReactDOM.render(<h1>react开发环境配置</h1>,document.querySelector('#root'));
